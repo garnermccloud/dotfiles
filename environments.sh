cbcac9f4176e7f44bca06fae22b7c8822a64fb24
Conor Branagan <conorbranagan@gmail.com>
2015-05-16 05:35:21 +0000

    Environment working for the VM too

--- environments.sh
+++ environments.sh
@@ -1,4 +1,3 @@
-
 if [ -e ~/dev/datadog/vm ]
 then
     VM=~/dev/datadog/vm
@@ -8,6 +7,24 @@ then
     export GOROOT=/usr/local/opt/go
     export GOBIN=$GOROOT/bin
     export DDGO=$GOPATH/src/github.com/DataDog/dd-go
+
+    # path
+    export PATH=$PATH:$VM/dogweb/node_modules/.bin/:$GOBIN
+fi
+
+if [ -e /home/vagrant/workspace ]
+then
+    VM=/home/vagrant/workspace
+
+    # go env
+    export GOPATH=$VM/go
+    export GOROOT=/usr/local/opt/go
+    export GOBIN=$GOROOT/bin
+    export DDGO=$GOPATH/src/github.com/DataDog/dd-go
+
+    # python
+    export DOGWEB_DEFAULT_CONFIG_PATH=$VM/dogweb/development.ini
+    source ~/dogweb/python/bin/activate
 
     # path
     export PATH=$PATH:$VM/dogweb/node_modules/.bin/:$GOBIN
